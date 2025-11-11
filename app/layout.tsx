f2bbf1acdcc22c0ec8eae6c6d727b534b54f7044
yakob <yysiyzx07@gmail.com>
2025-11-11 19:01:45 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${inter.className} anialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
